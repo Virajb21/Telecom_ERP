20232db38c35a5cbf8f1cf371953e38ed834f5f7
Atharva <atharvachirputkar333@gmail.com>
2025-09-06 19:43:39 +0530

    Project initialized..

---
+++ src/main/java/com/atharva/erp_telecom/ErpTelecomApplication.java
@@ -0,0 +1,13 @@
+package com.atharva.erp_telecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpTelecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpTelecomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atharva/erp_telecom/ErpTelecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atharva.erp_telecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpTelecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
