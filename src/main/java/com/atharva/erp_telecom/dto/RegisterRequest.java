dfc2088c66d1d51f50458e5db637c46fe96236a9
Atharva <atharvachirputkar333@gmail.com>
2025-09-15 19:20:30 +0530

    Added DTO's for new user registration req-response

---
+++ src/main/java/com/atharva/erp_telecom/dto/RegisterRequest.java
@@ -0,0 +1,56 @@
+package com.atharva.erp_telecom.dto;
+
+import jakarta.persistence.Entity;
+
+import java.util.Set;
+
+
+public class RegisterRequest {
+    private String userName;
+    private String password;
+    private String userFirstName;
+    private String userLastName;
+    private Set<String> roles;
+
+    public RegisterRequest() {}
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserFirstName() {
+        return userFirstName;
+    }
+
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+}
